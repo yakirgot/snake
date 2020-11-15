e6e3d53f7c46c9b6e11210f8a3ff50300bc84027
Yakir <got.yakir@gmail.com>
2020-11-15 12:23:53 +0100

    refactor: calculate board by snake units
    this is a preparation for making the food locations array

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,7 +1,27 @@
 export class Configuration {
-  static boardWidthInPixels = 600;
-  static boardHeightInPixels = 405;
+  static boardWidthInSnakeUnits = 24;
+  static boardHeightInSnakeUnits = 16;
   static snakePieceSizeInPixels = 20;
-  static movementDelayInMs = 300;
+  static movementDelayInMs = 250;
   static snakeRectGap = 5;
+
+  static get boardWidthInPixels(): number {
+    const { snakePieceSizeWithGap, boardWidthInSnakeUnits } = Configuration;
+    const boardWidth = snakePieceSizeWithGap * boardWidthInSnakeUnits;
+
+    return boardWidth;
+  }
+
+  static get boardHeightInPixels(): number {
+    const { snakePieceSizeWithGap, boardHeightInSnakeUnits } = Configuration;
+    const boardHeight = snakePieceSizeWithGap * boardHeightInSnakeUnits;
+
+    return boardHeight;
+  }
+
+  private static get snakePieceSizeWithGap(): number {
+    const { snakePieceSizeInPixels, snakeRectGap } = Configuration;
+
+    return snakePieceSizeInPixels + snakeRectGap;
+  }
 }
