ce3df8fa33d2419d0e875a52891122a11f6f39c9
Yakir <got.yakir@gmail.com>
2022-12-25 20:22:21 +0100

    feat: add more settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,11 +1,18 @@
-export default {
-	/**
-	 * number of x-axis snake parts
-	 */
-	boardWidth: 44,
+const settings = {
 	/**
 	 * number of y-axis snake parts
 	 */
 	boardHeight: 30,
+	/**
+	 * number of x-axis snake parts
+	 */
+	boardWidth: 44,
+	snakeGapInPx: 2,
+	snakeInitialLength: 3,
 	snakePartSizeInPx: 14,
+	get snakeSizeWithGap() {
+		return this.snakePartSizeInPx + this.snakeGapInPx;
+	},
 } as const;
+
+export default settings;
