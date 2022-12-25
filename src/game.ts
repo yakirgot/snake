761c5e7e286639843aba275f9007c1c0635090e9
Yakir <got.yakir@gmail.com>
2022-12-25 21:55:08 +0100

    feat: move snake every n ms

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,10 @@
-import { cleanUpBoard, placeSnakeOnStartingPoint } from "@/board";
-import { resetSnake } from "@/snake";
+import { cleanBoard } from "@/board";
+import { moveSnake, placeSnakeOnStartingPoint, resetSnake } from "@/snake";
+import settings from "@/settings";
 
 const gameInProgressClass = "game-in-progress";
 let startButton: Element;
+let moveSnakeIntervalId: number | undefined;
 
 export function initGame() {
 	const possibleStartButton = document.querySelector(
@@ -25,11 +27,18 @@ export function initGame() {
 function startGame() {
 	placeSnakeOnStartingPoint();
 
+	moveSnakeIntervalId = window.setInterval(
+		moveSnake,
+		settings.snakeIntervalInMs,
+	);
+
 	setTimeout(endGame, 5000);
 }
 
 function endGame() {
 	startButton.classList.remove(gameInProgressClass);
+	window.clearInterval(moveSnakeIntervalId);
+	moveSnakeIntervalId = undefined;
 
 	resetSnake();
 	cleanUpBoard();
