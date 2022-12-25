a6ec303bf8e135347f349627b75abd85281cbdf0
Yakir <got.yakir@gmail.com>
2022-12-25 20:25:40 +0100

    feat: initiate board with three snake parts

--- src/board.ts
+++ src/board.ts
@@ -1,5 +1,6 @@
 import settings from "@/settings";
-import { placeSnakePart } from "@/snake";
+import { SnakePosition } from "@/snake-position.type";
+import { addSnakePart, drawSnakeOnBoard } from "@/snake";
 
 export let canvasElement: HTMLCanvasElement;
 export let canvasContext: CanvasRenderingContext2D;
@@ -28,12 +29,12 @@ export function setupBoard() {
 }
 
 function setBoardSize() {
-	const { boardWidth, boardHeight, snakePartSizeInPx } = settings;
+	const { boardWidth, boardHeight, snakeSizeWithGap } = settings;
 
-	canvasElement.width = boardWidth * snakePartSizeInPx;
+	canvasElement.width = boardWidth * snakeSizeWithGap;
 	canvasElement.style.width = `${canvasElement.width}px`;
 
-	canvasElement.height = boardHeight * snakePartSizeInPx;
+	canvasElement.height = boardHeight * snakeSizeWithGap;
 	canvasElement.style.height = `${canvasElement.height}px`;
 }
 
@@ -45,19 +46,39 @@ export function cleanUpBoard() {
 }
 
 export function placeSnakeOnStartingPoint() {
-	const { boardWidth, boardHeight, snakePartSizeInPx } = settings;
-	const halfSnakeSize = snakePartSizeInPx / 2;
+	const { snakeInitialLength, snakeSizeWithGap } = settings;
+	const snakeStartingPosition = getStartingPoint();
 
-	const boardWidthInPx = boardWidth * snakePartSizeInPx;
+	for (let index = 0; index < snakeInitialLength; index++) {
+		const xPositionCompensation = index * snakeSizeWithGap;
+		const snakeXPosition = snakeStartingPosition[0] + xPositionCompensation;
+
+		const snakePosition: SnakePosition = [
+			snakeXPosition,
+			snakeStartingPosition[1],
+		];
+
+		addSnakePart(snakePosition);
+	}
+
+	drawSnakeOnBoard();
+}
+
+function getStartingPoint(): SnakePosition {
+	const { boardWidth, boardHeight, snakeSizeWithGap } = settings;
+
+	const boardWidthInPx = boardWidth * snakeSizeWithGap;
 	const quarterScreenX = boardWidthInPx / 4;
-	const xCoordinate = quarterScreenX - halfSnakeSize;
 
-	const boardHeightInPx = boardHeight * snakePartSizeInPx;
+	const boardHeightInPx = boardHeight * snakeSizeWithGap;
 	const middleScreenY = boardHeightInPx / 2;
+	const halfSnakeSize = snakeSizeWithGap / 2;
 	const yCoordinate = middleScreenY - halfSnakeSize;
 
-	const normalizedX = Math.floor(xCoordinate);
+	const normalizedX = Math.floor(quarterScreenX);
 	const normalizedY = Math.floor(yCoordinate);
 
-	placeSnakePart(normalizedX, normalizedY);
+	const snakeStartingPosition: SnakePosition = [normalizedX, normalizedY];
+
+	return snakeStartingPosition;
 }
