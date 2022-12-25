9448fa321480a615de014d4850eccbc863999407
Yakir <got.yakir@gmail.com>
2022-12-25 20:24:46 +0100

    feat: support multiple snake pieces

--- src/snake.ts
+++ src/snake.ts
@@ -1,18 +1,34 @@
 import settings from "@/settings";
 import { canvasContext } from "@/board";
+import { SnakePosition } from "@/snake-position.type";
 
 const canvasFillStyle = window
 	.getComputedStyle(document.documentElement)
 	.getPropertyValue("--color-dark-slate-gray");
+let snakePositions: SnakePosition[] = [];
 
-export function placeSnakePart(xCoordinate: number, yCoordinate: number) {
-	const { snakePartSizeInPx } = settings;
+export function addSnakePart(position: SnakePosition) {
+	snakePositions.push(position);
+}
 
-	canvasContext.fillStyle = canvasFillStyle;
-	canvasContext.fillRect(
-		xCoordinate,
-		yCoordinate,
-		snakePartSizeInPx,
-		snakePartSizeInPx,
-	);
+export function popFirstSnakePart() {
+	snakePositions.shift();
+}
+
+export function resetSnake() {
+	snakePositions = [];
+}
+
+export function drawSnakeOnBoard() {
+	for (const snakePosition of snakePositions) {
+		const { snakePartSizeInPx } = settings;
+
+		canvasContext.fillStyle = canvasFillStyle;
+		canvasContext.fillRect(
+			snakePosition[0],
+			snakePosition[1],
+			snakePartSizeInPx,
+			snakePartSizeInPx,
+		);
+	}
 }
