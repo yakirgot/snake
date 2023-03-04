bc807f56288b04fbebc289a798f3fedde6435f0b
Yakir Gottesman <contact@better-front-end.com>
2023-03-04 16:13:29 +0200

    refactor: protect settings object from changes
    This should be refactored if we want to change the settings dynamically

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-const settings = {
+const settings = Object.freeze({
 	boardHeightInSnakeParts: 30,
 	boardWidthInSnakeParts: 44,
 	snakeGapInPx: 2,
@@ -14,6 +14,6 @@ const settings = {
 		return this.boardHeightInSnakeParts * this.snakeSizeWithGap;
 	},
 	snakeIntervalInMs: 250,
-} as const;
+} as const);
 
 export default settings;
