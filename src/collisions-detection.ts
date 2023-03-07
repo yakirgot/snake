5f71613075a691b2b5b8195a54ffa1663fc19473
Yakir Gottesman <contact@better-front-end.com>
2023-03-07 12:42:20 +0200

    feat: make collision detection more generic to support food logic

---
+++ src/collisions-detection.ts
@@ -0,0 +1,37 @@
+import { PartPosition } from "@/types/part-position";
+import settings from "@/settings";
+import { snakePositions } from "@/snake";
+
+export function detectWallCollision(partPosition: PartPosition) {
+	const { canvasWidthInPx, canvasHeightInPx, snakeSizeWithGap } = settings;
+
+	const [positionX, positionY] = partPosition;
+
+	if (positionX < 0 || positionY < 0) {
+		return true;
+	}
+
+	if (positionX + snakeSizeWithGap > canvasWidthInPx) {
+		return true;
+	}
+
+	return positionY + snakeSizeWithGap > canvasHeightInPx;
+}
+
+export function detectSnakeCollision(partPosition: PartPosition) {
+	const isCollision = snakePositions.some((snakePosition) =>
+		detectPartCollision(partPosition, snakePosition),
+	);
+
+	return isCollision;
+}
+
+export function detectPartCollision(
+	partPosition: PartPosition,
+	itemPosition: PartPosition,
+) {
+	const xCollision = partPosition[0] === itemPosition[0];
+	const yCollision = partPosition[1] === itemPosition[1];
+
+	return xCollision && yCollision;
+}
