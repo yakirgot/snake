c3a0cf5f572bb61892819ade9b1135a1ec3ae03d
Yakir Gottesman <contact@better-front-end.com>
2023-03-11 12:04:37 +0200

    refactor: separate game HTML to a different file

---
+++ src/game-html.ts
@@ -0,0 +1,19 @@
+export function gameHTML() {
+	return `
+		<div class="snake-game-container">
+			<h1 class="snake-game-title">Snake Game - <span data-game-points>0</span> Points</h1>
+
+			<canvas data-snake-game class="snake-game-canvas"></canvas>
+
+			<div class="snake-game-start-button-container">
+				<button
+					class="snake-game-start-button"
+					data-snake-game-start-button
+					tabindex="1"
+				>
+					Start!
+				</button>
+			</div>
+		</div>
+	`;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,12 @@
 import "the-new-css-reset/css/reset.css";
 import "@/styles/styles.scss";
 import { initGame } from "@/game-engine/game";
+import { gameHTML } from "@/game-html";
 
 const appElement = document.querySelector("#app");
 
 if (appElement) {
-	appElement.innerHTML = `
-		<div class="snake-game-container">
-			<h1 class="snake-game-title">Snake Game - <span data-game-points>0</span> Points</h1>
-
-			<canvas data-snake-game class="snake-game-canvas"></canvas>
-
-			<div class="snake-game-start-button-container">
-				<button
-					class="snake-game-start-button"
-					data-snake-game-start-button
-					tabindex="1"
-				>
-					Start!
-				</button>
-			</div>
-		</div>
-	`;
+	appElement.innerHTML = gameHTML();
 
 	initGame();
 }
