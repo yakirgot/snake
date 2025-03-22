48f77173a1ac7b1af0ba422737043e05ca9d0691
Yakir <got.yakir@gmail.com>
2025-03-22 14:16:29 +0100

    chore: install vitest lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,15 +5,11 @@ import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import vitest from "@vitest/eslint-plugin";
 
 /** @type {import('typescript-eslint').Config} */
 export default tsEslint.config(
 	{
-		plugins: {
-			json,
-			markdown,
-			css,
-		},
 		languageOptions: {
 			globals: {
 				...globals.node,
@@ -33,21 +29,33 @@ export default tsEslint.config(
 	},
 	{
 		files: ["**/*.json"],
+		plugins: { json },
 		ignores: ["package-lock.json"],
 		language: "json/json",
 		extends: [json.configs.recommended],
 	},
 	{
 		files: ["**/*.md"],
+		plugins: { markdown },
 		language: "markdown/commonmark",
 		extends: [markdown.configs.recommended],
 	},
 	{
 		files: ["**/*.css"],
+		plugins: { css },
 		language: "css/css",
 		extends: [css.configs.recommended],
 		rules: {
 			"css/prefer-logical-properties": "error",
+		},
+	},
+	{
+		files: ["**/*.spec.ts"],
+		plugins: { vitest },
+		extends: [vitest.configs.all],
+		rules: {
+			"vitest/prefer-expect-assertions": "off",
+			"vitest/consistent-test-filename": /.*\\.spec\\.ts?$/
 		},
 	},
 );
