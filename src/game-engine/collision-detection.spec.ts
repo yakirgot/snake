368236d1d3c8445f8fc74318a4bd44e7829a1b34
Yakir <got.yakir@gmail.com>
2025-04-20 15:29:47 +0200

    test: add collision detection tests

--- src/game-engine/collision-detection.spec.ts
+++ src/game-engine/collision-detection.spec.ts
@@ -1,17 +1,19 @@
-import { beforeEach, describe, expect, it } from "vitest";
-import { detectPartCollision } from "@/game-engine/collision-detection";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import {
+	detectPartCollision,
+	isSnakeCollision,
+} from "@/game-engine/collision-detection";
 import { container } from "tsyringe";
 import { GameSettings } from "@/settings";
 import { GameData } from "@/game-engine/game-data";
 
 describe("collision detection", () => {
-	const gameSettingsMock = new GameSettings();
-	gameSettingsMock.canvasHeightInSnakeParts = 3;
-	gameSettingsMock.canvasWidthInSnakeParts = 3;
+	const gameSettingsMock = vi.fn().mockReturnValue(new GameSettings());
+	const gameDataMock = vi.fn().mockReturnValue(new GameData());
 
 	beforeEach(() => {
-		container.register("GameSettings", { useValue: gameSettingsMock });
-		container.register("GameData", { useClass: GameData });
+		container.register("GameSettings", { useValue: gameSettingsMock() });
+		container.register("GameData", { useValue: gameDataMock() });
 	});
 
 	describe(detectPartCollision, () => {
@@ -26,6 +28,53 @@ describe("collision detection", () => {
 			expect(detectPartCollision([1, 1], [2, 1])).toBe(false);
 			expect(detectPartCollision([1, 1], [1, 2])).toBe(false);
 			expect(detectPartCollision([2, 1], [1, 2])).toBe(false);
+		});
+	});
+
+	describe(isSnakeCollision, () => {
+		beforeEach(() => {
+			const gameSettings = new GameSettings();
+			gameSettings.canvasWidthInSnakeParts = 2;
+			gameSettings.canvasHeightInSnakeParts = 2;
+			gameSettings.partSizeInPx = 1;
+			gameSettings.snakeGapInPx = 1;
+			gameSettingsMock.mockReturnValue(gameSettings);
+			container.register("GameSettings", { useValue: gameSettingsMock() });
+
+			const gameData = new GameData();
+			gameData.snakePositions.push([1, 1]);
+			gameDataMock.mockReturnValue(gameData);
+			container.register("GameData", { useValue: gameDataMock() });
+		});
+
+		it("should detect self collision", () => {
+			const isCollision = isSnakeCollision([1, 1]);
+
+			expect(isCollision).toBe(true);
+		});
+
+		it("should detect left wall collision", () => {
+			const isCollision = isSnakeCollision([-1, 0]);
+
+			expect(isCollision).toBe(true);
+		});
+
+		it("should detect top wall collision", () => {
+			const isCollision = isSnakeCollision([0, -1]);
+
+			expect(isCollision).toBe(true);
+		});
+
+		it("should detect right wall collision", () => {
+			const isCollision = isSnakeCollision([3, 0]);
+
+			expect(isCollision).toBe(true);
+		});
+
+		it("should detect bottom wall collision", () => {
+			const isCollision = isSnakeCollision([0, 3]);
+
+			expect(isCollision).toBe(true);
 		});
 	});
 });
