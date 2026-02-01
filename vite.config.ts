7cac75cfb81058240c059db3dbd423f2e079bf21
Yakir Gottesman <got.yakir@gmail.com>
2026-02-01 10:29:52 +0100

    feat: set base path for Vite to "/snake/"

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 export default defineConfig({
+	base: "/snake/",
 	resolve: {
 		alias: {
 			"@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
