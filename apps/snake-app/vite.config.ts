b2d3bc2a563fbc7c576bd607242b853a06b87a00
Yakir Gottesman <got.yakir@gmail.com>
2026-02-12 18:05:20 +0100

    build: set base URL to `/snake/` in Vite config

--- apps/snake-app/vite.config.ts
+++ apps/snake-app/vite.config.ts
@@ -4,6 +4,7 @@ import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
 
 export default defineConfig(() => ({
+	base: "/snake/",
 	root: import.meta.dirname,
 	cacheDir: "../../node_modules/.vite/apps/snake-app",
 	server: {
